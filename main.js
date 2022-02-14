8e47b9dc2ae4e1e864acaf6dde89a967dc39aeaa
Olufunto Oyewole <funti009@gmail.com>
2022-02-14 10:10:15 +0100

    adjust the css for the squares area

--- main.js
+++ main.js
@@ -2,5 +2,4 @@ const squares = document.querySelector('.squares');
 for (var i = 1; i < 365; i++) {
   const level = Math.floor(Math.random() * 3);  
   squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
-  // squares.innerHTML+=`<li data-level="${level}"></li>`;
 }
