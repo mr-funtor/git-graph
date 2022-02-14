51bb356270ebfde1bced40dde9a99a2e4f5f341d
Olufunto Oyewole <funti009@gmail.com>
2022-02-14 10:03:35 +0100

    adjusted the javascript

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const squares = document.querySelector('.squares');
 for (var i = 1; i < 365; i++) {
   const level = Math.floor(Math.random() * 3);  
-  squares.innerHTML+=`<li data-level="${level}"></li>`;
+  squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
+  // squares.innerHTML+=`<li data-level="${level}"></li>`;
 }
